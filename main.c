cf3fe245fedb4dad69262d07566cedb84ca1e11f
João Sousa <2231654@my.ipleiria.pt>
2023-12-14 17:35:21 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-void menuPrincipal();
-void total_ects ();
-
+void total_ects();
+void media();
+void percentagem_ects();
 
 void main()
 {
-    menuPrincipal();
-}
-
-void menuPrincipal()
-{
     char opcoes;
 
-    printf("Registar e consultar os dados dos estudantes");
+    printf("\nRegistar e consultar os dados dos estudantes");
     printf("\nRegistar e consultar os dados das unidades curriculares");
     printf("\nRegistar e consultar os dados das avalia\x87\x228es");
     printf("\nEstatísticas");
     printf("\nGuardar e ler de ficheiros binários os dados da aplicação");
-    printf("\nOpcao -> ");
+    printf("\nopcao -> ");
 
+
+    Estatisticas: total_ects();
+                  media();
+                  percentagem_ects();
+}
+
+total_ects()
+{
+    printf("Total de ECTS aprovados por um estudante: ");
+    scanf("%d",&total_ects);
+}
+
+media()
+{
+    printf("Média aritmética atual das classificações nas unidades curriculares aprovadas por um estudante: ");
+    scanf("%d",&media);
+}
+
+percentagem_ects()
+{
+    printf("Percentagem de ECTS aprovados em cada semestre letivo por um estudante: ");
+    scanf("%d",&percentagem_ects);
 }
