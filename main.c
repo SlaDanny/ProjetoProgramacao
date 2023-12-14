99c0208ff4b1239b3f4782fc1d0f7132f5b43c6d
João Sousa <2231654@my.ipleiria.pt>
2023-12-14 16:33:24 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 void menuPrincipal();
+void total_ects ();
+
 
 void main()
 {
-    DORITO
+    case Estatisticas: total_ects
+
 }
 
 void menuPrincipal()
 {
 
 }
+
+total_ects()
 
 
