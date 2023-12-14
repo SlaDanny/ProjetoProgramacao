65784fa8b426b8ab24334ffa6973c37675a94d07
João Sousa <2231654@my.ipleiria.pt>
2023-12-14 19:26:48 +0000

    guardado

--- main.c
+++ main.c
@@ -1,40 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
-void total_ects();
-void media();
-void percentagem_ects();
 
 void main()
 {
-    char opcoes;
+    int opcao, submenu, total_ects, percentagem_ects;
+    int case1 = 0, case2 = 0, case3 = 0, sair = 0;
+    float media;
 
-    printf("\nRegistar e consultar os dados dos estudantes");
-    printf("\nRegistar e consultar os dados das unidades curriculares");
-    printf("\nRegistar e consultar os dados das avalia\x87\x228es");
-    printf("\nEstatísticas");
-    printf("\nGuardar e ler de ficheiros binários os dados da aplicação");
-    printf("\nOpcao -> ");
+    printf("\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4");
+    printf("\n\t\t\t\tGestao da situacao curricular dos estudantes do curso TeSP PSI");
+    printf("\n\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4");
+    printf("\n 1 - Registar e consultar os dados dos estudantes");
+    printf("\n 2 - Registar e consultar os dados das unidades curriculares");
+    printf("\n 3 - Registar e consultar os dados das avaliacoes");
+    printf("\n 4 - Estatisticas");
+    printf("\n 5 - Guardar e ler de ficheiros binarios os dados da aplicacao");
+    printf("\n 6 - Sair");
+    printf("\n\n Opcao -> ");
+    scanf(" %d",&opcao);
 
 
-    Estatisticas: total_ects();
-                  media();
-                  percentagem_ects();
-}
+    do{
+        switch(opcao)
+        {
+            case 1:
+                    case1 = 1;
+                    printf("a: ");
+                    scanf("%d");
+                    break;
+            case 2:
+                    case2 = 1;
+                    printf("a: ");
+                    scanf("%d");
+                    break;
+            case 3:
+                    case3 = 1;
+                    printf("a: ");
+                    scanf("%d");
+                    break;
+            case 4:
+                    system("cls");
+                    if(case1 ==1 && case2 == 1 && case3 == 1)
+                    {
+                        do {
+                                printf("1 - Total de ECTS aprovados por um estudante");
+                                printf("\n2 - Media aritmetica atual das classificacoes nas unidades curriculares aprovadas por um estudante");
+                                printf("\n3 - Percentagem de ECTS aprovados em cada semestre letivo por um estudante");
+                                printf("\n\nOpcao -> ");
+                                scanf("%d",&submenu);
 
-total_ects()
-{
-    printf("Total de ECTS aprovados por um estudante: ");
-    scanf("%d",&total_ects);
-}
-
-media()
-{
-    printf("Média aritmética atual das classificações nas unidades curriculares aprovadas por um estudante: ");
-    scanf("%d",&media);
-}
-
-percentagem_ects()
-{
-    printf("Percentagem de ECTS aprovados em cada semestre letivo por um estudante: ");
-    scanf("%d",&percentagem_ects);
+                                switch(submenu)
+                                {
+                                    case 1:
+                                            system("cls");
+                                            printf("Total de ECTS aprovados por um estudante: ");
+                                            scanf("%d",&total_ects);
+                                            break;
+                                    case 2:
+                                            system("cls");
+                                            printf("Media aritmetica atual das classificacoes nas unidades curriculares aprovadas por um estudante: ");
+                                            scanf("%f",&media);
+                                            break;
+                                    case 3:
+                                            system("cls");
+                                            printf("Percentagem de ECTS aprovados em cada semestre letivo por um estudante: ");
+                                            scanf("%d",&percentagem_ects);
+                                            break;
+                                    case 4:
+                                            printf("Sair");
+                                            break;
+                                    default:
+                                            printf("Opcao invalida!\n");
+                                            break;
+                                }
+                            } while (submenu != 3);
+                    }
+                        else
+                        {
+                            printf("Erro ao acessar estas informacoes!");
+                            printf("\nE necessario registar as informacoes nas opcoes 1, 2, 3!");
+                        }
+                    break;
+            case 5:
+                    break;
+            case 6:
+                    sair = 1;
+                    break;
+            default:printf("Opcao invalida!");
+        }while(opcao != 6);
+    }while(!sair);
 }
